40b121358fb1c7d2b496da2dbcb289a499ad0f84
tnshine <tnshine@gmail.com>
2016-09-01 07:38:01 +0000

    challenge2 fix2

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,8 @@
 class Message < ActiveRecord::Base
     #名前は必須入力かつ20文字以内
     validates :name , length: { maximum: 20 } , presence: true
-    #年齢は0以上の数字だけ
-    validates :age , numericality: { only_integer: false, greater_than_or_equal_to: 0 }, if: :age
+    #年齢が入力された場合は、0以上の数字だけ許可する
+    validates :age , numericality: { only_integer: true, greater_than_or_equal_to: 0 }, if: :age
     #内容は必須入力かつ2文字以上30文字以下
     validates :body , length: { minimum: 2, maximum: 30 } , presence: true
 end
